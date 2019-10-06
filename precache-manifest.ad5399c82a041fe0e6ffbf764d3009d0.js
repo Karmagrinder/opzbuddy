50401fdf7d3033dbdc4bc101c0d2ffeda1155030
Karmagrinder <bharat.bhushan.konka@gmail.com>
2019-10-06 16:51:08 +0200

    Updates

---
+++ precache-manifest.ad5399c82a041fe0e6ffbf764d3009d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7204ee2dd4c03a52aed03a1523789714",
+    "url": "/karmagrinder/index.html"
+  },
+  {
+    "revision": "9fb2b2228c0104d2dd4c",
+    "url": "/karmagrinder/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "d70a5961b71513daa107",
+    "url": "/karmagrinder/static/css/main.626ebaa7.chunk.css"
+  },
+  {
+    "revision": "9fb2b2228c0104d2dd4c",
+    "url": "/karmagrinder/static/js/2.f4f06e32.chunk.js"
+  },
+  {
+    "revision": "d70a5961b71513daa107",
+    "url": "/karmagrinder/static/js/main.ca5f0fcc.chunk.js"
+  },
+  {
+    "revision": "5a14c566be5e39d2eace",
+    "url": "/karmagrinder/static/js/runtime-main.250b7f24.js"
+  },
+  {
+    "revision": "fba70051610a98790b0cc4d626654305",
+    "url": "/karmagrinder/static/media/sineWave.fba70051.svg"
+  }
+]);
