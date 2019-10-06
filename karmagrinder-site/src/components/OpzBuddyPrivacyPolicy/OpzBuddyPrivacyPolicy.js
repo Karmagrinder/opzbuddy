a876bc7d6b10feef873d0e20ac5f93fc44f398c7
Karmagrinder <bharat.bhushan.konka@gmail.com>
2019-10-06 15:47:55 +0200

    Added privacy policy.

--- karmagrinder-site/src/components/OpzBuddyPrivacyPolicy/OpzBuddyPrivacyPolicy.js
+++ karmagrinder-site/src/components/OpzBuddyPrivacyPolicy/OpzBuddyPrivacyPolicy.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './OpzBuddyPrivacyPolicy.scss';
+import Media from 'react-bootstrap/Media';
 
 const OpzBuddyPrivacyPolicy = props => (
-	<div>This is a component called OpzBuddyPrivacyPolicy.</div>
+	<div className="Privacy-policy">
+		<Media>
+			<Media.Body>
+				<h5>OP-Z Buddy Privacy Policy</h5>
+				<p>
+					OP-Z Buddy app, does not collect or store user's data other than the internal memory of the device on which application is running (i.e user's personal device).
+				</p>
+				<p>
+					App listens to MIDI data received from a specific type of device (OP-Z Synth by Teenage Engineering) and visualizes the parameters.
+				</p>
+				<p>
+					App also requires microphone, camera, and internal/external memory access on user's device, this is to provide audio/video recording and data backup(locally) features.
+    			</p>
+				<p>
+					OP-Z Buddy does not access any other audio device other Teenage Engineering's OP-Z.		
+				</p>
+			</Media.Body>
+		</Media>
+	</div>
 );
 
 // todo: Unless you need to use lifecycle methods or local state,
--- karmagrinder-site/src/components/OpzBuddyPrivacyPolicy/index.js
+++ karmagrinder-site/src/components/OpzBuddyPrivacyPolicy/index.js
@@ -1,3 +1,4 @@
 import OpzBuddyPrivacyPolicy from './OpzBuddyPrivacyPolicy';
+import './OpzBuddyPrivacyPolicy.scss';
 
 export default OpzBuddyPrivacyPolicy;
