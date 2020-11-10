32bec5e8d91efeae6385202589d3df7bca15065a
Bharat Konka <bharat.bhushan.konka@gmail.com>
2020-11-10 13:17:01 +0100

    updated the guide.

--- karmagrinder-site/src/components/OpzBuddy/OpzBuddy.js
+++ karmagrinder-site/src/components/OpzBuddy/OpzBuddy.js
@@ -32,9 +32,22 @@ const OpzBuddy = props => (
 				<h5>2. RECORDER:</h5>
 				<p>
 					To enable audio/video recording functionality you must connect OP-Z via USB-Cable. When the App detects OP-Z as audio device, the recording features will be enabled.
-					Audio is recorded in WAV(stereo) format with 16 BIT and 44.1K hz sampling frequency. Video is recorded in MPEG4 format and audio is encoded in AAC with same bit depth and sampling frequency as audio-recorder.
-					<br /> 
-    			</p>
+					Audio is recorded in WAV(stereo) format with 16 BIT and 44.1K hz sampling frequency. 
+					Video is recorded in MPEG4 format and audio is encoded in AAC with same bit depth and sampling frequency as audio-recorder.
+					<br /> 					
+				</p>
+				<h6>2.1 Android Q (10) Compatibility:</h6>
+				<p>
+					On Android Q and above, recorded media will  be saved to the SD-Card or internal storage in case the SD-Card is not present. 
+					Audio will be saved under Music folder, and videos will be saved under Video folder.
+					<br />
+					The audio source mapping is changed in Android Q ( for some reason... thanks for sleepless nights Google), in the previous versions of android the external-mic( OP-Z Audio) was mapped to "MIC".
+					At least on Pixel-2 phones running Android Q the external-mic is mapped to "Voice-Recognition".
+					<br />
+					In android-Q the default audio source will be set to "VOICE_RECOGNITION", in case this results in no audio in your recordings, there is a drop-down menu to switch audio-source.
+					Usually it should be "MIC" or "VOICE_RECOGNITION".
+					<br/>
+				</p>
 				<br />
 				<h5>3. DATA BACKUP</h5>
 				<p>					
@@ -42,18 +55,20 @@ const OpzBuddy = props => (
 					You can create backup via direct folder copy or create a zip-archive.
 					<br /> 
 				</p>
+				<h6>3.1 Android Q (10) Compatibility:</h6>
+				<p>
+					On Android Q and above OP-Z data backups will be saved to the "Download" folder.
+					<br />
+					Also due to security related limitations enforced by Android Q, the app can no longer gain access to OP-Z disk, therefore user must grant permission manually by selecting the correct OP-Z Disk root.
+					<br />					
+				</p>
 				<br />
-				<h5>4. Issues with Android Q</h5>
+				<h5>4. Unsupported devices</h5>
 				<p>					
-					Android API version 29 (Android Q) has deprecated the old storage methods, this effects how audio/video recorder feature will work. 
-					I am currently working to resolve this but the documentation for the new methods is very poor, therefore the development is taking time.
-					Also the disk-backup feature is disabled for Android Q, this is again for the reason stated above.
-
-					Audio/ Video recording should still function on Android Q, but the files will be saved in a sandbox folder(internal storage/Android/data/com.opzdisplay),
-					which is somewhat tedious to find. I recomend using a 3rd party file explorer. In meanwhile I will try to fix this.
-
-					If the app does not work for you contact me via e-mail, I will refund your payment.
-					<br /> 
+					Samsung S20: Samsung has done something with how USB-Audio drivers work. Audio and Video recorded with OP-Z Buddy will result in no audio.
+					Rest of the functionality should still work.
+					<br />
+					In case the app does not work on your phone, please contact me via mail, I will provide a refund. 
 				</p>
 				<br />
 			</Media.Body>
