8dbaa3dd14aecaf90e381c39369c9185343a257f
Bharat Konka <bharat.bhushan.konka@gmail.com>
2020-11-10 16:17:32 +0100

    Changed styling.

--- karmagrinder-site/src/components/OpzBuddy/OpzBuddy.js
+++ karmagrinder-site/src/components/OpzBuddy/OpzBuddy.js
@@ -2,75 +2,97 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './OpzBuddy.scss';
 import Media from 'react-bootstrap/Media';
+import Card from 'react-bootstrap/Card';
 
 const OpzBuddy = props => (
 	<div>
 		<Media>
 			<Media.Body>
-				<h3>OP-Z Buddy App Help</h3>
-				<p>
-					This app provides display, audio/video recording and data backup functionality for Teenage Engineering OP-Z.
-					<br/>
-					<br/> 
-				</p>
-				<h5>1. CONNECTING OP-Z TO PHONE</h5>
-				<h6>a) Via Bluetooth:</h6>
-				<p>
-					You can use one of the many available Bluetooth-MIDI apps on the Google App store for connecting OP-Z to your phone via bluetooth.
-					<br />
-					Launch the app only after OP-Z is connected to the phone.
-					<br />					
-				</p>
+				<Card bg="dark" text="light">
+					<Card.Title className="title-text">OP-Z Buddy App Help</Card.Title>
+					<Card.Body>
+						<Card.Text className="general-text">
+							This app provides display, audio/video recording and data backup functionality for Teenage Engineering OP-Z.
+						</Card.Text>
+					</Card.Body>
+					<Card.Body>
+						<Card bg="dark" text="light">
+							<Card.Title className="title-text" >1. CONNECTING OP-Z TO PHONE</Card.Title>
+							<Card.Subtitle className="title-text">a) Via Bluetooth:</Card.Subtitle>
+							<Card.Body>
+								<Card.Text className="general-text">
+									You can use one of the many available Bluetooth-MIDI apps on the Google App store for connecting OP-Z to your phone via bluetooth.
+									<br />
+									Launch the app only after OP-Z is connected to the phone.
+									<br />
+								</Card.Text>
+								
+							</Card.Body>
+							<Card.Subtitle className="title-text">b) Via USB-Cable:</Card.Subtitle>
+							<Card.Body>
+								<Card.Text className="general-text">
+									Wait for 2-3 seconds after OP-Z has booted up and then connect OP-Z to your phone via USB-OTG adapter.
+									Also ensure that your phone supports USB-OTG functionality.
+								</Card.Text>
+							</Card.Body>								
+						</Card>
+						<Card bg="dark" text="light">
+							<Card.Title className="title-text">2. RECORDER:</Card.Title>
+							<Card.Body>
+								<Card.Text className="general-text">
+									To enable audio/video recording functionality you must connect OP-Z via USB-Cable. When the App detects OP-Z as audio device, the recording features will be enabled.
+									Audio is recorded in WAV(stereo) format with 16 BIT and 44.1K hz sampling frequency. 
+									Video is recorded in MPEG4 format and audio is encoded in AAC with same bit depth and sampling frequency as audio-recorder.
+								</Card.Text>
 
-				<h6>b) Via USB-Cable:</h6>
-				<p>
-					Wait for 2-3 seconds after OP-Z has booted up and then connect OP-Z to your phone via USB-OTG adapter.
-					Also ensure that your phone supports USB-OTG functionality.
-					<br /> 
-				</p>
-				<br />
-				<h5>2. RECORDER:</h5>
-				<p>
-					To enable audio/video recording functionality you must connect OP-Z via USB-Cable. When the App detects OP-Z as audio device, the recording features will be enabled.
-					Audio is recorded in WAV(stereo) format with 16 BIT and 44.1K hz sampling frequency. 
-					Video is recorded in MPEG4 format and audio is encoded in AAC with same bit depth and sampling frequency as audio-recorder.
-					<br /> 					
-				</p>
-				<h6>2.1 Android Q (10) Compatibility:</h6>
-				<p>
-					On Android Q and above, recorded media will  be saved to the SD-Card or internal storage in case the SD-Card is not present. 
-					Audio will be saved under Music folder, and videos will be saved under Video folder.
-					<br />
-					The audio source mapping is changed in Android Q ( for some reason... thanks for sleepless nights Google), in the previous versions of android the external-mic( OP-Z Audio) was mapped to "MIC".
-					At least on Pixel-2 phones running Android Q the external-mic is mapped to "Voice-Recognition".
-					<br />
-					In android-Q the default audio source will be set to "VOICE_RECOGNITION", in case this results in no audio in your recordings, there is a drop-down menu to switch audio-source.
-					Usually it should be "MIC" or "VOICE_RECOGNITION".
-					<br/>
-				</p>
-				<br />
-				<h5>3. DATA BACKUP</h5>
-				<p>					
-					When OP-Z is connected in content disk-mode, app will enable the data-backup feature. Backups of OP-Z data are stored in the device internal memory. 
-					You can create backup via direct folder copy or create a zip-archive.
-					<br /> 
-				</p>
-				<h6>3.1 Android Q (10) Compatibility:</h6>
-				<p>
-					On Android Q and above OP-Z data backups will be saved to the "Download" folder.
-					<br />
-					Also due to security related limitations enforced by Android Q, the app can no longer gain access to OP-Z disk, therefore user must grant permission manually by selecting the correct OP-Z Disk root.
-					<br />					
-				</p>
-				<br />
-				<h5>4. Unsupported devices</h5>
-				<p>					
-					Samsung S20: Samsung has done something with how USB-Audio drivers work. Audio and Video recorded with OP-Z Buddy will result in no audio.
-					Rest of the functionality should still work.
-					<br />
-					In case the app does not work on your phone, please contact me via mail, I will provide a refund. 
-				</p>
-				<br />
+							</Card.Body>
+							<Card.Subtitle className="title-text">2.1 Android Q (10) Compatibility:</Card.Subtitle>
+							<Card.Body>
+								<Card.Text className="general-text">
+									On Android Q and above, recorded media will  be saved to the SD-Card or internal storage in case the SD-Card is not present.
+									Audio will be saved under Music folder, and videos will be saved under Video folder.
+									<br />
+									The audio source mapping is changed in Android Q ( for some reason... thanks for sleepless nights Google), in the previous versions of android the external-mic( OP-Z Audio) was mapped to "MIC".
+									At least on Pixel-2 phones running Android Q the external-mic is mapped to "Voice-Recognition".
+									<br />
+									In android-Q the default audio source will be set to "VOICE_RECOGNITION", in case this results in no audio in your recordings, there is a drop-down menu to switch audio-source.
+									Usually it should be "MIC" or "VOICE_RECOGNITION".
+									<br />
+								</Card.Text>
+							</Card.Body>
+						</Card>
+						<Card bg="dark" text="light">
+							<Card.Title className="title-text">3. DATA BACKUP</Card.Title>
+							<Card.Body>
+								<Card.Text className="general-text">
+									When OP-Z is connected in content disk-mode, app will enable the data-backup feature. Backups of OP-Z data are stored in the device internal memory. 
+									You can create backup via direct folder copy or create a zip-archive.
+								</Card.Text>
+
+							</Card.Body>
+							<Card.Subtitle className="title-text">3.1 Android Q (10) Compatibility:</Card.Subtitle>
+							<Card.Body>
+								<Card.Text className="general-text">
+									On Android Q and above OP-Z data backups will be saved to the "Download" folder.
+									<br />
+									Also due to security related limitations enforced by Android Q, the app can no longer gain access to OP-Z disk, therefore user must grant permission manually by selecting the correct OP-Z Disk root.
+									<br />
+								</Card.Text>
+							</Card.Body>
+						</Card>
+						<Card bg="dark" text="light">
+							<Card.Title className="title-text">4. Unsupported devices</Card.Title>
+							<Card.Body>
+								<Card.Text className="general-text">
+									Samsung S20: Samsung has done something with how USB-Audio drivers work. Audio and Video recorded with OP-Z Buddy will result in no audio.
+									Rest of the functionality should still work.
+									<br />
+									In case the app does not work on your phone, please contact me via mail, I will provide a refund.
+								</Card.Text>
+							</Card.Body>							
+						</Card>
+					</Card.Body>
+				</Card>	
 			</Media.Body>
 		</Media>
 
--- karmagrinder-site/src/components/OpzBuddyPrivacyPolicy/OpzBuddyPrivacyPolicy.js
+++ karmagrinder-site/src/components/OpzBuddyPrivacyPolicy/OpzBuddyPrivacyPolicy.js
@@ -2,24 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './OpzBuddyPrivacyPolicy.scss';
 import Media from 'react-bootstrap/Media';
+import Card from 'react-bootstrap/Card';
 
 const OpzBuddyPrivacyPolicy = props => (
 	<div className="Privacy-policy">
 		<Media>
-			<Media.Body>
-				<h4>OP-Z Buddy Privacy Policy</h4>
-				<p>
-					OP-Z Buddy app, does not collect or store user's data other than the internal memory of the device on which application is running (i.e user's personal device).
-				</p>
-				<p>
-					App listens to MIDI data received from a specific type of device (OP-Z Synth by Teenage Engineering) and visualizes the parameters.
-				</p>
-				<p>
-					App also requires microphone, camera, and internal/external memory access on user's device, this is to provide audio/video recording and data backup(locally) features.
-    			</p>
-				<p>
-					OP-Z Buddy does not access any other audio device other Teenage Engineering's OP-Z.		
-				</p>
+			<Media.Body style={{height:'100vh'}}>
+				<Card bg="dark" text="light">
+					<Card.Title className="title-text"> OP-Z Buddy Privacy Policy </Card.Title>
+					<Card.Body>
+						<Card.Text className="general-text">							
+								OP-Z Buddy app, does not collect or store user's data other than the internal memory of the device on which application is running (i.e user's personal device).
+				
+								App listens to MIDI data received from a specific type of device (OP-Z Synth by Teenage Engineering) and visualizes the parameters.
+				
+								App also requires microphone, camera, and internal/external memory access on user's device, this is to provide audio/video recording and data backup(locally) features.
+    			
+								OP-Z Buddy does not access any other audio device other Teenage Engineering's OP-Z.				
+						</Card.Text>
+					</Card.Body>
+				</Card>				
 			</Media.Body>
 		</Media>
 	</div>
